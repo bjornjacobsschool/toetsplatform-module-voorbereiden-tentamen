72a68d82872230e09ffda113cac36a79eeabb846
Michel Douma <M.douma@student.han.nl>
2018-06-01 12:04:57 +0200

    todo url specificeren voor opslaan tentamen post request

--- src/main/java/nl/han/toetsplatform/module/voorbereiden/applicationlayer/TentamenSamenstellen.java
+++ src/main/java/nl/han/toetsplatform/module/voorbereiden/applicationlayer/TentamenSamenstellen.java
@@ -4,10 +4,13 @@ import nl.han.toetsplatform.module.voorbereiden.exceptions.GatewayCommunicationE
 import nl.han.toetsplatform.module.voorbereiden.models.Tentamen;
 import nl.han.toetsplatform.module.voorbereiden.serviceagent.GatewayServiceAgent;
 
+import javax.inject.Inject;
+
 public class TentamenSamenstellen implements ITentamenSamenstellen {
 
     private GatewayServiceAgent _gatewayServiceAgent;
 
+    @Inject
     public TentamenSamenstellen(GatewayServiceAgent gatewayServiceAgent)
     {
         this._gatewayServiceAgent = gatewayServiceAgent;
@@ -16,6 +19,7 @@ public class TentamenSamenstellen implements ITentamenSamenstellen {
     public void opslaan(Tentamen tentamen) throws GatewayCommunicationException {
 
         // tentamen versturen naar gateway
+        // todo URL specificeren voor post request opslaan tentamen
         this._gatewayServiceAgent.post("", tentamen);
 
         // tentamen opslaan in lokale database
