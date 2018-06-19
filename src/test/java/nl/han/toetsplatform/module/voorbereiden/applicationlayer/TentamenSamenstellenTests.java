7f3353668a78df75a6e61187679363ad7dd2a653
Bjorn Jacobs <BGM.Jacobs@student.han.nl>
2018-06-19 17:15:39 +0200

    Testen

--- src/test/java/nl/han/toetsplatform/module/voorbereiden/applicationlayer/TentamenSamenstellenTests.java
+++ src/test/java/nl/han/toetsplatform/module/voorbereiden/applicationlayer/TentamenSamenstellenTests.java
@@ -1,11 +1,13 @@
 package nl.han.toetsplatform.module.voorbereiden.applicationlayer;
 
 import nl.han.toetsapplicatie.apimodels.dto.SamengesteldTentamenDto;
+import nl.han.toetsapplicatie.apimodels.dto.VersieDto;
 import nl.han.toetsapplicatie.apimodels.dto.VragenbankVraagDto;
 import nl.han.toetsplatform.module.voorbereiden.data.TentamenDao;
 import nl.han.toetsplatform.module.voorbereiden.data.VragenDao;
 import nl.han.toetsplatform.module.voorbereiden.exceptions.GatewayCommunicationException;
 import nl.han.toetsplatform.module.voorbereiden.serviceagent.IGatewayServiceAgent;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +17,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -60,8 +67,18 @@ public class TentamenSamenstellenTests {
     }
 
     @Test
-    public void getVragenTest(){
+    public void getVragenTest() throws GatewayCommunicationException {
+        VragenbankVraagDto[] serverVragen = getServerVragen();
+        List<VragenbankVraagDto> localVragen = new ArrayList<>();
+        localVragen.add(serverVragen[1]);
 
+        doReturn(localVragen).when(_vragenDao).getVragen();
+        doReturn(serverVragen).when(_gatewayServiceAgentMock).get(Mockito.any(), Mockito.any());
+
+        List<VragenbankVraagDto> realVragen = _sut.getVragen();
+
+        verify(_vragenDao,times(2)).insertVraag(Mockito.any());
+        assertEquals(localVragen, realVragen );
     }
 
     private VragenbankVraagDto[] getServerVragen(){
@@ -69,7 +86,15 @@ public class TentamenSamenstellenTests {
         vragen[0] = new VragenbankVraagDto();
         vragen[1] = new VragenbankVraagDto();
         vragen[2] = new VragenbankVraagDto();
+        VersieDto versieDto = new VersieDto();
+        versieDto.setNummer(1);
+        vragen[0].setVersie(versieDto);
+        vragen[1].setVersie(versieDto);
+        vragen[2].setVersie(versieDto);
 
-
+        vragen[0].setId(UUID.randomUUID());
+        vragen[1].setId(UUID.randomUUID());
+        vragen[2].setId(UUID.randomUUID());
+        return vragen;
     }
 }
